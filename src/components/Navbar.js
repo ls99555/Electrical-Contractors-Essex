722a856ec99db212379ea10ce4b676b32a235f9a
Luke Stevens <lukestevens@Lukes-Laptop.local>
2025-01-31 09:30:59 +0000

    Make nav scss and complete styling plus slight alternations to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.scss';
 
 const Navbar = () => {
     return (
-        <nav>
-         <ul className='navbar'>
+        
+        <nav className='navbar'>
+            <div className='navbar-logo'>
+               <Link to='/'>Electrical Contractors Essex</Link>
+            </div>
+         <ul className='navbar-links'>
             <li> <Link to='/'>Home</Link></li>
             <li> <Link to='/team'>Meet The Team</Link></li>
             <li> <Link to='/services'>Services</Link></li>
